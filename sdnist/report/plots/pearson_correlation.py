d494a2f5a08e78fad2c65ce96dda19ff0a25bbb8
Karan Bhagat <kb.in.triangulum@gmail.com>
2023-02-17 13:52:17 +0530

    Fix colorbar size of correlation plots

--- sdnist/report/plots/pearson_correlation.py
+++ sdnist/report/plots/pearson_correlation.py
@@ -38,12 +38,10 @@ class PearsonCorrelationPlot:
         cd = cd.sort_index()
         cd = cd.abs()
         fig = plt.figure(figsize=(6, 6), dpi=100)
-        max_val = cd.to_numpy().max()
-        # v_max = max_val if max_val > 0.15 else 0.15
         v_max = 0.15
         plt.imshow(cd, cmap='Blues', interpolation='none', vmin=0, vmax=v_max)
         im_ratio = cd.shape[0] / cd.shape[1]
-        cbar = plt.colorbar(fraction=0.047 * im_ratio)
+        plt.colorbar(fraction=0.04 * im_ratio)
         plt.xticks(range(cd.shape[1]), cd.columns)
         plt.xticks(rotation=90)
         plt.yticks(range(cd.shape[0]), cd.index)
