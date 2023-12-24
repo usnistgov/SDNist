06d6805822f078894a2fae368a55f3e1482a9e97
Karan Bhagat <kb.in.triangulum@gmail.com>
2023-12-24 18:24:59 +0530

    Update index package to report progress for the gui

--- sdnist/index/__init__.py
+++ sdnist/index/__init__.py
@@ -8,6 +8,9 @@ from sdnist.gui.constants import (
     METAREPORT_DIR_PREFIX,
     ARCHIVE_DIR_PREFIX
 )
+
+from sdnist.report.helpers.progress_status import (
+    ProgressStatus, ProgressLabels)
 import sdnist.strs as strs
 
 
@@ -65,14 +68,30 @@ column_order = [
     DEID_DATA_ID
 ]
 
+def get_metadata_files(data_root: str):
+    data_root = Path(data_root)
+    json_files = data_root.glob('**/*.json')
+    metadata_files = [f for f in json_files
+                      if (REPORT_DIR_PREFIX not in str(f) and
+                           ARCHIVE_DIR_PREFIX not in str(f) and
+                           METAREPORT_DIR_PREFIX not in str(f))]
+    # keep only files that has csv file
+    metadata_files = [f for f in metadata_files
+                      if Path(str(f).replace('.json', '.csv')).exists()]
+    return metadata_files
 
-def index(data_root: str):
+def index(progress: ProgressStatus, data_root: str):
     """
     Creates an index csv file indexing all the
     deid dataset available in the data_root or subdirectories
     of the data_root:
     data_root: path to the root directory of the deid datasets
     """
+    out_path = Path(data_root, 'index.csv')
+
+    progress.update(str(out_path),
+                    ProgressLabels.STARTED)
+
     data_dir = Path(__file__).parent.parent.parent
     TARGET_DATA_DIR = Path(data_dir, 'diverse_communities_data_excerpts')
 
@@ -90,53 +109,62 @@ def index(data_root: str):
         data_dict = json.load(f)
 
     index_rows = []
-    data_root = Path(data_root)
-    json_files = data_root.glob('**/*.json')
 
     # remove all files that SDNIST_DER in their path
-    metadata_files = [f for f in json_files
-                      if (REPORT_DIR_PREFIX not in str(f) and
-                           ARCHIVE_DIR_PREFIX not in str(f) and
-                           METAREPORT_DIR_PREFIX not in str(f))]
-    for metadata_file in metadata_files:
+    metadata_files = get_metadata_files(data_root)
+
+    for i, metadata_file in enumerate(metadata_files):
         with open(metadata_file, 'r') as f:
             metadata = json.load(f)
+
         metadata = metadata['labels']
         #TODO FIX PATH
-        metadata[DATA_PATH] = str(Path(*metadata_file.parts[-4:]))\
+        metadata[DATA_PATH] = str(metadata_file)\
             .replace('.json', '.csv')
         metadata[LABELS_PATH] = str(metadata_file)
 
         # get report path
         reports_path = Path(metadata_file.parent, 'reports')
-        report_dirs = [p
-                       for p in reports_path.iterdir()
-                       if 'SDNIST_DER' in str(p)]
+        report_json_path = Path(reports_path, 'report.json')
+        if reports_path.exists():
+            report_dirs = [p
+                           for p in reports_path.iterdir()
+                           if 'SDNIST_DER' in str(p)]
 
-        report_dirs = [d for d in report_dirs
-                      if metadata_file.stem in str(d)]
-        report_dirs = [str(r) for r in report_dirs]
-        report_dirs = sorted(report_dirs, reverse=True)
+            report_dirs = [d for d in report_dirs
+                          if metadata_file.stem in str(d)]
+            report_dirs = [str(r) for r in report_dirs]
+            report_dirs = sorted(report_dirs, reverse=True)
 
-        metadata[REPORT_PATH] = str(report_dirs[0])
-        report_json_path = Path(report_dirs[0], 'report.json')
-        with open(report_json_path, 'r') as f:
-            report_json = json.load(f)
-        target_df = None
-        if metadata[TARGET_DATASET] == 'ma2019':
-            target_df = ma_df
-        elif metadata[TARGET_DATASET] == 'tx2019':
-            target_df = tx_df
-        elif metadata[TARGET_DATASET] == 'national2019':
-            target_df = na_df
+            metadata[REPORT_PATH] = str(report_dirs[0])
 
-        features = metadata[FEATURES_LIST].split(', ')
-        sub_target_df = target_df[features]
+            report_json_path = Path(report_dirs[0], 'report.json')
+            k = 1
+            while not report_json_path.exists() and k < len(report_dirs):
+                report_json_path = Path(report_dirs[k], 'report.json')
+                k += 1
 
-        metadata[FEATURE_SPACE_SIZE] = metadata[FEATURE_SPACE_SIZE]
-        metadata[ONLY_NUMERICAL_RESULTS] = \
-            report_json[strs.CONTAINS_ONLY_NUMERICAL_RESULTS]
-        index_rows.append(pd.DataFrame(metadata, index=[0]))
+        if report_json_path.exists():
+            with open(report_json_path, 'r') as f:
+                report_json = json.load(f)
+            target_df = None
+            if metadata[TARGET_DATASET] == 'ma2019':
+                target_df = ma_df
+            elif metadata[TARGET_DATASET] == 'tx2019':
+                target_df = tx_df
+            elif metadata[TARGET_DATASET] == 'national2019':
+                target_df = na_df
+
+            features = metadata[FEATURES_LIST].split(', ')
+            sub_target_df = target_df[features]
+
+            metadata[FEATURE_SPACE_SIZE] = metadata[FEATURE_SPACE_SIZE]
+            metadata[ONLY_NUMERICAL_RESULTS] = \
+                report_json[strs.CONTAINS_ONLY_NUMERICAL_RESULTS]
+            index_rows.append(pd.DataFrame(metadata, index=[0]))
+        progress.update(str(out_path),
+                        ProgressLabels.PROCESSING_FILES,
+                        str(i))
         # get the target dataset
     index_df = pd.concat(index_rows, ignore_index=True)
 
@@ -147,6 +175,10 @@ def index(data_root: str):
     index_df = index_df.sort_values(by=c_in_idx[:5])
     index_df = index_df.reset_index(drop=True)
 
+    index_df.to_csv(out_path, index=False)
+
+    progress.update(str(out_path),
+                    ProgressLabels.CREATING_INDEX)
     return index_df
 
 
