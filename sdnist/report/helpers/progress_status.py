fd786e94ab127bf2f6422649b0c9b2e68062861d
Karan Bhagat <kb.in.triangulum@gmail.com>
2023-12-24 18:21:38 +0530

    Update progress_status to record progress for metareport, index and archive generation as well

--- sdnist/report/helpers/progress_status.py
+++ sdnist/report/helpers/progress_status.py
@@ -1,13 +1,19 @@
 import queue
-from typing import List
+from typing import List, Optional
 from multiprocessing import Lock
 from enum import Enum
 
 from queue import Queue
 
 
+class ProgressType(Enum):
+    REPORTS = 'reports'
+    METAREPORT = 'metareport'
+    ARCHIVE = 'archive'
+    INDEX = 'index'
+
 class ProgressLabels(Enum):
-    # only an indicator that report generation is started
+    # indicator that report generation is started
     STARTED = 'STARTED'
     UNIVARIATES = 'Univariates'
     CORRELATIONS = 'Correlations'
@@ -20,22 +26,77 @@ class ProgressLabels(Enum):
     APPARENT_MATCH_DISTRIBUTION = 'Apparent Match Distribution'
     SAVING_REPORT_DATA = 'Saving Report Data'
     CREATING_EVALUATION_REPORT = 'Creating Evaluation Report'
+    # METAREPORT progress indicators
+    COPYING_REPORTS = 'Copying Reports'
+    CREATING_METAREPORT = 'Creating Meta Report'
+    # Index and Archive progess indicators
+    PROCESSING_FILES = 'Processing Files'
+    CREATING_INDEX = 'Creating Index'
+    CREATING_ARCHIVE = 'Creating Archive'
 
+report_progress = [
+    ProgressLabels.STARTED,
+    ProgressLabels.UNIVARIATES,
+    ProgressLabels.CORRELATIONS,
+    ProgressLabels.K_MARGINAL,
+    ProgressLabels.PROPENSITY_MSE,
+    ProgressLabels.LINEAR_REGRESSION,
+    ProgressLabels.PCA,
+    ProgressLabels.INCONSISTENCIES,
+    ProgressLabels.UNIQUE_EXACT_MATCHES,
+    ProgressLabels.APPARENT_MATCH_DISTRIBUTION,
+    ProgressLabels.SAVING_REPORT_DATA,
+    ProgressLabels.CREATING_EVALUATION_REPORT
+]
+
+metareport_progress = [
+    ProgressLabels.STARTED,
+    ProgressLabels.COPYING_REPORTS,
+    ProgressLabels.CORRELATIONS,
+    ProgressLabels.UNIQUE_EXACT_MATCHES,
+    ProgressLabels.PCA,
+    ProgressLabels.LINEAR_REGRESSION,
+    ProgressLabels.CREATING_METAREPORT
+]
+
+progress_labels_dict = {
+    ProgressType.REPORTS: report_progress,
+    ProgressType.METAREPORT: metareport_progress,
+    ProgressType.INDEX: [ProgressLabels.STARTED,
+                         ProgressLabels.PROCESSING_FILES,
+                         ProgressLabels.CREATING_INDEX],
+    ProgressType.ARCHIVE: [ProgressLabels.STARTED,
+                           ProgressLabels.PROCESSING_FILES,
+                           ProgressLabels.CREATING_ARCHIVE]
+}
+
+max_progress_dict = {
+    ProgressType.REPORTS: len(report_progress),
+    ProgressType.METAREPORT: len(metareport_progress),
+    ProgressType.INDEX: 1,
+    ProgressType.ARCHIVE: 1
+}
 
 class ProgressStatus:
-    def __init__(self):
+    def __init__(self,
+                 progress_type: ProgressType,
+                 ):
         # -1 because STARTED label is not a progress number.
-        self.single_report_max_progress = \
-            len(list(ProgressLabels)) - 1
-        self.reports = dict()
+        self.progress_type = progress_type
+        self.single_item_max_progress = max_progress_dict[progress_type] - 1
+
+        self.items = dict()
         self.max_progress = 0
         self.last_progress = 0
         self.current_progress = 0
         self.completed = False
-        self.completed_reports: List[str] = []
+        self.completed_items: List[str] = []
         self.progress_label = ''
         self.updates = Queue()
         self.lock = Lock()
+
+    def get_progress_type(self):
+        return self.progress_type
 
     def get_max_progress(self):
         return self.max_progress
@@ -56,19 +117,25 @@ class ProgressStatus:
         return self.progress_label
 
     def get_progress_percent(self):
-        return (self.current_progress/self.max_progress)*100
+        prog =  (self.current_progress/self.max_progress)*100
+        return prog
 
-    def get_completed_reports(self):
-        return self.completed_reports
+    def get_completed_items(self):
+        return self.completed_items
 
-    def add_report(self, report_name: str):
+    def add_item(self, item_name: str,
+                   max_progress: Optional[int] = None):
         with self.lock:
             self.completed = False
-            self.reports[report_name] = (0, None)
-            self.max_progress = self.single_report_max_progress * len(self.reports)
+            self.items[item_name] = (0, None)
+            if max_progress is not None:
+                self.single_item_max_progress = max_progress
+            self.max_progress = self.single_item_max_progress * len(self.items)
 
     def is_busy(self):
-        return self.current_progress < self.max_progress
+        busy = (self.current_progress < self.max_progress
+                or not self.updates.empty())
+        return busy
 
     def is_completed(self):
         res = self.completed
@@ -89,19 +156,40 @@ class ProgressStatus:
                     break
             return updates
 
-    def update(self, report_name: str, label: ProgressLabels):
+    def update(self,
+               item_name: str,
+               label: ProgressLabels,
+               extra_label: str = ''):
         with self.lock:
-            progress = list(ProgressLabels).index(label)
-            self.reports[report_name] = (progress, label)
+            if len(extra_label) and extra_label.isnumeric():
+                progress = int(extra_label)
+            else:
+                prog_list = progress_labels_dict[self.progress_type]
+                progress = prog_list.index(label)
+
             self.last_progress = self.current_progress
-            self.current_progress = sum([x[0] for x in self.reports.values()])
             self.progress_label = label
-            progress_percent = (progress
-                                / self.single_report_max_progress) \
-                * 100
-            self.updates.put((report_name, label, progress_percent))
-            if label == ProgressLabels.CREATING_EVALUATION_REPORT:
-                self.completed_reports.append(report_name)
+            if label in [
+                ProgressLabels.CREATING_ARCHIVE,
+                ProgressLabels.CREATING_INDEX]:
+                self.current_progress = self.current_progress + 1
+                progress_percent = 100
+            else:
+                self.items[item_name] = (progress, label)
+                self.last_progress = self.current_progress
+                self.current_progress = sum([x[0] for x in self.items.values()])
+
+                progress_percent = (progress
+                                    / self.single_item_max_progress) \
+                    * 100
+
+            self.updates.put((item_name, label, extra_label, progress_percent))
+            if label in [
+                ProgressLabels.CREATING_EVALUATION_REPORT,
+                ProgressLabels.CREATING_METAREPORT,
+                ProgressLabels.CREATING_ARCHIVE,
+                ProgressLabels.CREATING_INDEX]:
+                self.completed_items.append(item_name)
             if (self.current_progress > 0
                     and self.current_progress == self.max_progress):
                 self.completed = True
